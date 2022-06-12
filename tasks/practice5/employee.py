3a01ccecd38c826494e89c14da96dba34301cef3
Maximallnyi <avilov_2003@mail.ru>
2022-06-12 15:52:56 +0300

    practice 5

--- tasks/practice5/employee.py
+++ tasks/practice5/employee.py
@@ -39,6 +39,12 @@ class Employee:
         """
 
         # пиши свой код здесь
+        self.name=name
+        self.position=position
+        if isinstance(salary,int):
+            self._salary=salary
+        else:
+            raise ValueError
 
     def get_salary(self) -> int:
         """
@@ -46,6 +52,7 @@ class Employee:
         """
 
         # пиши свой код здесь
+        return self._salary
 
     def __eq__(self, other: object) -> bool:
         """
@@ -56,6 +63,16 @@ class Employee:
         """
 
         # пиши свой код здесь
+        if isinstance(self,Employee) and isinstance(other,Employee):
+            try:
+                if(get_position_level(self.position)==get_position_level(other.position)):
+                    return True
+                else:
+                    return False
+            except NoSuchPositionError:
+                raise ValueError
+        raise TypeError
+
 
     def __str__(self):
         """
@@ -64,7 +81,8 @@ class Employee:
         """
 
         # пиши свой код здесь
-
+        string=f'name: {self.name} position: {self.position}'
+        return string
     def __hash__(self):
         return id(self)
 
@@ -83,7 +101,8 @@ class Developer(Employee):
         """
 
         # пиши свой код здесь
-
+        super().__init__(name,self.position, salary)
+        self.language=language
 
 class Manager(Employee):
     """
@@ -98,3 +117,4 @@ class Manager(Employee):
         """
 
         # пиши свой код здесь
+        super().__init__(name, self.position, salary)
